bf55468ed7dcf308c7e92431afccb22d4fcf88f1
Mauro Rocco <mauro.rocco@cap4lab.com>
2020-04-28 12:08:57 +0200

    Add django 3 and python 3.8 to setup

--- setup.py
+++ setup.py
@@ -43,10 +43,12 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
